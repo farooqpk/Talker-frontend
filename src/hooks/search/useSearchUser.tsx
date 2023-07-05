4787ca61f8053d069f42025626396154c9cb732f
Ummar Farooq <farooqcherur@gmail.com>
2023-07-05 23:39:11 +0530

    impliented search functionality and optimized input using debouncing

--- src/hooks/search/useSearchUser.tsx
+++ src/hooks/search/useSearchUser.tsx
@@ -1,15 +1,17 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-export const useRandomUsersForSearchbar = () => {
-    
+export const useSearchUser = (searchName: string) => {
   return useQuery(
-    "getRandomUsersForSearch",
+    ["searchuser", searchName],
     async () => {
       try {
         return await axios.get(
-          `${import.meta.env.VITE_SERVER_URL}/getRandomUsersForSearch`,{
-            withCredentials:true
+          `${
+            import.meta.env.VITE_SERVER_URL
+          }/searchUser?userName=${searchName}`,
+          {
+            withCredentials: true,
           }
         );
       } catch (error: any) {
@@ -23,8 +25,9 @@ export const useRandomUsersForSearchbar = () => {
       }
     },
     {
-      retry: false,
-      staleTime: Infinity,
+     enabled:!!searchName,
+     staleTime: 0,
+     retry:false
     }
   );
 };
