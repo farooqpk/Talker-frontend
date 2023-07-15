db600bcb2915cd615363154cd3ab6aa7633a98f4
Ummar Farooq <farooqcherur@gmail.com>
2023-07-15 12:05:49 +0530

    designed chat-page content like chat messages with time etc..

--- src/components/chatHome/chatContent.tsx
+++ src/components/chatHome/chatContent.tsx
@@ -1,19 +1,26 @@
 export const ChatContent = () => {
-  const a = [
-    1, 2, 2, 3, 32, 3, 3, 3, 3, 33, 3, 3, 3, 3, 3, 33, 39, 3, 3, 3, 34, 4, 4, 4,
-    4, 4, 4, 4, 3, 3, 4, 3, 43, 4, 3, 43, 1, 2, 2, 3, 32, 3, 3, 3, 3, 33, 3, 3, 3, 3, 3, 33, 39, 3, 3, 3, 34, 4, 4, 4,
-    4, 4, 4, 4, 3, 3, 4, 3, 43, 4, 3, 43
-  ];
   return (
     <>
       <div className=" text-white h-full md:w-[70%] break-all">
-       
-       {a.map((item)=>{
-        return (
-          <p>{item}</p>
-        )
-       })}
-      
+
+        <div className="chat chat-start">
+          <div className="chat-header flex gap-3 md:text-sm">
+            Other person
+            <time className="text-xs md:text-sm opacity-50">2 hours ago</time>
+          </div>
+          <div className="chat-bubble md:text-lg">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Seen</div>
+        </div>
+
+        <div className="chat chat-end">
+          <div className="chat-header flex gap-3 md:text-sm">
+            Me
+            <time className="text-xs md:text-sm opacity-50">2 hours ago</time>
+          </div>
+          <div className="chat-bubble md:text-lg">You were the Chosen One!</div>
+          <div className="chat-footer opacity-50">Seen</div>
+        </div>
+        
       </div>
     </>
   );
