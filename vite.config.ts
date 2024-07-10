1cec4cd948efd88de95fee9a576c3ba6b21bc4bb
farooqpk <farooqcherur@gmail.com>
2024-07-10 07:26:27 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import path from "path";
 
-const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: 'prompt',
-  includeAssets: ['favicon.ico', "apple-touch-icon.png", "masked-icon.svg"],
-  manifest: {
-    name: "Talker",
-    short_name: "Talker",
-    description: "Talk with your friends and family using Talker",
-    icons: [
-      {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'any'
-      },
-      {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any'
-      },
-      {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
-      },
-      {
-        src: '/maskable_icon.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      }
-    ],
-    theme_color: "#000000",
-    background_color: "#000000",
-    display: "standalone",
-    scope: '/',
-    start_url: "/",
-    orientation: 'portrait'
-  }
-};
-
 export default defineConfig({
-  plugins: [react(), svgr(), VitePWA(manifestForPlugin)],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
